bd3ba4aeabdc1baad72c9e653584ff31764cd1c8
qztweII <terencexfwu@gmail.com>
2026-02-13 09:07:18 +1100

    Improved main
    Enabled multiple orders to be saved as files, now shows discount applied in the receipt

--- main.py
+++ main.py
@@ -9,18 +9,36 @@ while not doneOrdering: #Taking Order
         order.append(itemToOrder)
 
 price = 0
+discount = 0
 for item in order: #Discount mechanism
     price += 5
 if price >= 20:
     price = price * 0.8
+    discount = 20
 elif price >= 10:
     price = price * 0.9
+    discount = 10
 
-f = open("Orders.txt", "x") #Save order to system
+#Open a new receipt file
+openedFile = False
+attempts = 0
+while not openedFile:
+    try:
+        fileName = f"Orders{attempts}.txt"
+        f = open(fileName, "x")
+    except:
+        attempts += 1
+    else:
+        openedFile = True
+
+#Save order to system
+f.write(name + "\n")
 for i in order:
     f.write(i + "\n")
 f.write(str(price) + "\n")
+if discount > 0:
+    f.write(f"Discount applied: {discount}%")
 f.close()
 
-f = open("Orders.txt") #Display receipt
+f = open(fileName) #Display receipt
 print(f.read())
